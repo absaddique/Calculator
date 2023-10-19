3dbeefa3ae5b66f553d3f12542791d5eddff6f21
absaddique <abubakarsaddique973@gmail.com>
2023-10-19 20:22:32 +0500

    files added

---
+++ index.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        massage: "kindly enter your first no",
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        massage: "kindly enter your first no",
+    },
+    {
+        type: "list",
+        name: "Operator",
+        choices: ["+", "-", "*", "/"],
+        massage: "Select Operator",
+    },
+]);
+const { numberOne, numberTwo, Operator } = answers;
+if (numberOne && numberTwo && Operator) {
+    let result = 0;
+    if (Operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (Operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (Operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    if (Operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
